acfeb8130c695f2fa0feae65a6b48bd326c079e6
Rodolfo Oliveira <rodolfo_b_o@hotmail.com>
2017-01-01 21:56:11 -0200

    Fixing javacript Thanks God

--- app/assets/javascripts/jquery.app.js
+++ app/assets/javascripts/jquery.app.js
@@ -6,71 +6,7 @@
    File Description:Main JS file of the template
 */
 
-
 !function($) {
-    "use strict";
-
-
-    var ContactForm = function() {
-        this.$contactForm = $("#contact-form"),
-        this.$errorMessages = $("#err-form"),
-        this.$confirmMessage = $("#success-form")
-    };
-    //contact form submit handler
-    ContactForm.prototype.submitForm = function(e) {
-        var $this = this;
-        $this.$errorMessages.fadeOut('slow'); // reset the error messages (hides them)
-
-        var data_string = $this.$contactForm.serialize(); // Collect data from form
-        $.ajax({
-            type: "POST",
-            url: $this.$contactForm.attr('action'),
-            data: data_string,
-            timeout: 6000,
-            cache: false,
-            crossDomain: false,
-            error: function (request, error) {
-                $this.$errorMessages.html('An error occurred: ' + error + '');
-            },
-            success: function () {
-                $this.$confirmMessage.show(500).delay(4000).animate({
-                    height: 'toggle'
-                    }, 500, function () {
-                    }
-                );
-            }
-        });
-        return false;
-    },
-
-    ContactForm.prototype.init = function () {
-        var $this = this;
-        //initializing the contact form
-        this.$contactForm.parsley().on('form:submit', function() {
-            $this.submitForm();
-            return false;
-        });
-    },
-    $.ContactForm = new ContactForm, $.ContactForm.Constructor = ContactForm
-
-}(window.jQuery),
-
-
-function($) {
-    "use strict";
-
-    var SubscribeForm = function () {
-        this.$subscribeForm = $("#subscribe-form")
-    };
-    SubscribeForm.prototype.init = function () {
-        var $this = this;
-        //initializing the form using ajaxChimp
-        this.$subscribeForm.ajaxChimp({});
-    },
-    $.SubscribeForm = new SubscribeForm, $.SubscribeForm.Constructor = SubscribeForm
-}(window.jQuery),
-
-function($) {
     "use strict";
 
     var Page = function() {
@@ -94,7 +30,7 @@ function($) {
             $this.$topSection.css('height', windowHeight);
 
             //init sticky
-            $this.$stickyElem.sticky({topSpacing: 0});
+            $("#sticky-nav").sticky({topSpacing: 0});
         });
 
         //Handling the resize event
@@ -106,11 +42,13 @@ function($) {
         //Handling the scroll event
         $(window).scroll(function(){
             if ($(this).scrollTop() > 100) {
-                $this.$backToTop.fadeIn();
+              console.log("fadeIn");
+                $('#back-to-top').fadeIn();
             } else {
-                $this.$backToTop.fadeOut();
+              console.log("fadeOut");
+                $('#back-to-top').fadeOut();
             }
-        }); 
+        });
 
         //on click on navbar - Smooth Scroll To Anchor (requires jQuery Easing plugin)
         this.$topNavbar.on('click', function(event) {
@@ -119,24 +57,16 @@ function($) {
                 $('html, body').stop().animate({
                     scrollTop: $($anchor.attr('href')).offset().top - 0
                 }, 1500, 'easeInOutExpo');
-                event.preventDefault();    
+                event.preventDefault();
             }
         });
 
         //back-to-top button
-        $this.$backToTop.click(function(){
+        $('#back-to-top').click(function(){
+          console.log('click');
             $("html, body").animate({ scrollTop: 0 }, 1000);
             return false;
         });
-
-
-        //init contact app if contact form added in a page
-        if(this.$contactForm.length>0)
-            $.ContactForm.init();
-
-        //init subscribe app if form is added in a page
-        if(this.$subscribeForm.length>0)
-            $.SubscribeForm.init();
     },
     //init
     $.Page = new Page, $.Page.Constructor = Page
